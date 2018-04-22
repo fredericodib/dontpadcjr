1d9ec16c1e8f9d08587e47b2fb0306d320694b53
Frederico Dib <fredericodib@cjr.org.br>
2018-04-22 01:43:09 -0300

    heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,6 +86,8 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.middleware.use ChatActionCable  
+  config.web_socket_server_url = "wss://dontpad-cjr.herokuapp.com/"
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
