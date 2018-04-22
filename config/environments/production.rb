1a8aac1931a202d69ad6c7345a0b5b03b7cca8a0
Frederico Dib <fredericodib@cjr.org.br>
2018-04-22 11:58:53 -0300

    heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,8 +41,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = 'wss://dontpad-cjr.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [ 'http://dontpad-cjr.herokuapp.com', /http:\/\/dontpad-cjr.herokuapp.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
@@ -86,8 +86,6 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
-  config.middleware.use ChatActionCable  
-  config.web_socket_server_url = "wss://dontpad-cjr.herokuapp.com/"
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
